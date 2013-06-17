c5fa2176a9795dbcf70481f43ecd1135eb7ff70d
Cameron Dutro <camertron@gmail.com>
2013-06-16 18:14:53 -0700

    Science and Health HTML formatter now puts <em> tags around _underscored_ phrases and joins lines with <br />

--- lib/cskit/formatters/science_health/science_health_html_formatter.rb
+++ lib/cskit/formatters/science_health/science_health_html_formatter.rb
@@ -4,6 +4,28 @@ module CSKit
   module Formatters
     module ScienceHealth
       class ScienceHealthHtmlFormatter < ScienceHealthPlainTextFormatter
+
+        # _word_
+        ITALICS_REGEX = /_([a-zA-Z0-9\-\.,\"\'\?]+)_/
+
+        protected
+
+        def format_lines(lines, citation_line)
+          detect_italics(super)
+        end
+
+        def detect_italics(lines)
+          lines.map do |line|
+            line.gsub(ITALICS_REGEX) do |match|
+              "<em>#{$1}</em>"
+            end
+          end
+        end
+
+        def separator
+          options[:separator] || "<br />"
+        end
+
       end
     end
   end
